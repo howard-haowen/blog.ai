a5f7c16c40ee363f8d449358f305b55491e8f0b4
howard-haowen <howard-haowen@users.noreply.github.com>
2021-01-18 13:28:15 +0000

    deploy: af7083edb38d225f1fd3e9e3ebe6fac9af2c5fa7

--- assets/js/search.js
+++ assets/js/search.js
@@ -47,7 +47,7 @@ jtd.onReady = function(ready) {
 
 function initSearch() {
     var request = new XMLHttpRequest();
-    request.open('GET', '/hownwhendunit.ai/assets/js/search-data.json', true);
+    request.open('GET', '/blog.ai/assets/js/search-data.json', true);
   
     request.onload = function(){
       if (request.status >= 200 && request.status < 400) {
